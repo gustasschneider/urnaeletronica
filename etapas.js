e361cf033f809a7c589a8bd6a0513362517fec01
gustaschneider <gustavo.farias@genesisgroup.com.br>
2020-10-22 17:45:09 -0300

    urna eletronica em javascript

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'Comunista',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 17122,
+                nome: 'Beltrano',
+                partido: 'Cadeado',
+                fotos: [
+                    {url: '17122.jpg', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 24,
+                nome: 'Teste de Tal',
+                partido: 'Comunista',
+                vice: 'Teste2 vice',
+                fotos: [
+                    {url: 'teste.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'test2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 12,
+                nome: 'Arrombs',
+                partido: 'Cadeado',
+                vice: 'Arrombs2 vice',
+                fotos: [
+                    {url: 'teste3.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'teste4.jpg', legenda: 'Vice-Prefeito', small: true},
+                ]
+            }
+        ]
+    }
+]
