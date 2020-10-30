06de31b2ab829c2a0f24f69e160f96a7c290bdb8
gustaschneider <gustavo.farias@genesisgroup.com.br>
2020-10-30 11:21:18 -0300

    Tela Fim e Salvando Voto

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
 
     let etapa = etapas[etapaAtual];
     let numeroHtml = '';
+    numero  = '';
+    votoBranco = false;
 
     for(let i = 0; i < etapa.numeros; i++){
 
@@ -52,7 +56,11 @@ function atualizaInterface() {
 
         let fotosHtml = '';
         for(let i in candidato.fotos){
-            fotosHtml += `<div class="d-1-imagem"><img src="../imagens/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+            if(candidato.fotos[i].small){
+                fotosHtml += `<div class="d-1-imagem small"><img src="../imagens/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+            }else {
+                fotosHtml += `<div class="d-1-imagem"><img src="../imagens/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+            }
         }
         lateral.innerHTML = fotosHtml;
     }else{
@@ -81,15 +89,46 @@ function clicou(n) {
 }
 
 function branco() {
-    alert("Clicou em BRANCO");
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block'
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso-grande pisca">VOTO EM BRANCO</div>';
+    lateral.innerHTML = '';
 }
 
 function corrige() {
-    alert("Clicou em CORRIGE");
+    comecarEtapa();
 }
 
 function confirma() {
-    alert("Clicou em CONFIRMA");
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+           etapa: etapas[etapaAtual].titulo,
+           voto: 'branco'
+        });
+    }else if (numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if(votoConfirmado === true){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        }else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso-gigante">FIM</div>';
+            console.log(votos);
+        }
+    }
 }
 
 comecarEtapa();
